f0e21d8b44142eb8d134ae62ba33f58f2ad0d03f
Nazmul Hassan <nazmulhasannoman111@gmail.com>
2020-01-22 01:27:49 +0600

    commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+urlpatterns = [
+    path('',views.indexView,name="home"),
+    path('dashboard/',views.dashboardView,name="dashboard_url"),
+    path('login/',LoginView.as_view(),name="login_url"),
+    path('resgister/',views.registerView,name="register_url"),
+    path('logout/',LogoutView.as_view(next_page='dashboard'),name="logout"),
+]
